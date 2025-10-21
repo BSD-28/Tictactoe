ae6c291a106282ef07b5a005cd88a96057f9300b
fagunrew <agungfauziawan@gmail.com>
2025-10-21 17:41:48 +0700

    fix: homepage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router";
-import LoginPage from "./views/LoginPage.jsx";
 import Homepage from "./views/Homepage.jsx";
 import LandingPage from "./views/LandingPage.jsx";
 
