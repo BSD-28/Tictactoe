ccf18a57104970ff6c6e017481017ee187512919
Raditya Wisnu Herdinanto <radityawisnu071@gmail.com>
2025-10-22 10:48:56 +0700

    ai styling

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Homepage from "./views/Homepage.jsx";
 import LandingPage from "./views/LandingPage.jsx";
 import GamePage from "./views/GamePage.jsx";
+import VsAi from "./views/VsAi.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")).render(
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<Homepage />} />
         <Route path="/game" element={<GamePage />} />
+        <Route path="/vs-ai" element={<VsAi/>}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>
