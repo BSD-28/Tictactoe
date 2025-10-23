cac0d84b5b87ff8293a3bfb20a38dfbf21c78af4
fagunrew <agungfauziawan@gmail.com>
2025-10-23 10:17:36 +0700

    fix:socket

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
+import BASE_URL from "../../constant";
 
-export const socket = io("http://localhost:3000", {
+export const socket = io(BASE_URL, {
     autoConnect: false,
     transports: ["websocket"],
     withCredentials: true,
